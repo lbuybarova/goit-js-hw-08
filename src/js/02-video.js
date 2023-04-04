d55c40f0f3460666702f9437664c452457ecac80
Elizaveta Buibarova <liza.buybarova@gmail.com>
2023-04-04 22:59:11 +0300

    Друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORTIME = "videoplayer-current-time";
+const currentTime = localStorage.getItem(STORTIME);
+
+if (currentTime){
+    player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle((evt) => {
+    localStorage.setItem(STORTIME, evt.seconds)    
+}, 1000) );
