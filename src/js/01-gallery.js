a2202c778c7b3ad94f84ab94a5085fdcf6775e93
Elizaveta Buibarova <liza.buybarova@gmail.com>
2023-04-04 22:40:44 +0300

    Перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+function createCards(images) {
+    return images.map(({preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>        
+        `;
+    })
+        .join(' '); 
+}
+
+const cards = createCards(galleryItems);
+gallery.insertAdjacentHTML('beforeend', cards);
+
+const lightbox = new SimpleLightbox('.gallery a', {    
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 100
+});
